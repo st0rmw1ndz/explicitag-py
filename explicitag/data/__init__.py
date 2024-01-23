c7c2a071516c20400f6443bb01a1d1d50384458f
frosty <inthishouseofcards@gmail.com>
2024-01-22 20:18:14 -0500

    fix formatting

--- explicitag/data/__init__.py
+++ explicitag/data/__init__.py
@@ -2,10 +2,6 @@ from dataclasses import dataclass
 from enum import Enum
 from pathlib import Path
 
-explicit_words_path = Path(__file__).resolve().parent / "explicit_words.txt"
-with explicit_words_path.open(mode="r") as f:
-    explicit_words = f.read().splitlines()
-
 
 class MP4Rating(Enum):
     CLEAN = 0
@@ -18,3 +14,9 @@ class MP4Tags:
     title = "©nam"
     artist = "©ART"
     rating = "rtng"
+
+
+explicit_words_path = Path(__file__).resolve().parent / "explicit_words.txt"
+
+with explicit_words_path.open(mode="r") as f:
+    explicit_words = f.read().splitlines()
